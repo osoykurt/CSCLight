7f54b02cd6dd1fe28c1755ff843e9f650e23ace0
CoTrehiou <corentin.trehiou@wanadoo.fr>
2017-11-29 19:58:05 -0500

    nothing2

--- app/src/main/java/ca/uqac/csclight/MainActivity.java
+++ app/src/main/java/ca/uqac/csclight/MainActivity.java
@@ -2,12 +2,19 @@ package ca.uqac.csclight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        String  name,surname,telephone,email;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
